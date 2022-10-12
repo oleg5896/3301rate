7c7fae64b027f8ff1daa5ef8ea60e8ddb404d55f
Кожевников Олег <oleg.kozhevnikov@instatsport.com>
2022-10-12 20:21:27 +0400

    qwwe

---
+++ bot.php
@@ -0,0 +1,19 @@
+<?php
+require 'config.php';
+
+$input = file_get_contents('php://input');
+function getTelegramApi($method, $opt = null){
+  $url = API_URL . TOKEN . DIRECTORY_SEPARATOR . $method;
+  if ($opt){
+    $url .= http_build_query($opt);
+  }
+  $request = file_get_contents($url);
+  return json_decode($request, true);
+}
+
+function setWebhook($set=true){
+  $url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  getTelegramApi('setWebhook',[
+    'url' => $set ? $url : ''
+  ]);
+}
---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+const TOKEN = '5751240123:AAE74uVslQnsc205DsRwLv2F21Th6iIPgGE';
+const API_URL = 'https://api.telegram.org/bot';
